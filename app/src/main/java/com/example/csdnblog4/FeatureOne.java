f2014f3856de31197907bc7911a62b6ddc634935
orange <huangcheng@cheyaoshi.com>
2016-02-18 19:41:39 +0800

    motiefis FeatueOne

--- app/src/main/java/com/example/csdnblog4/FeatureOne.java
+++ app/src/main/java/com/example/csdnblog4/FeatureOne.java
@@ -1,7 +1,14 @@
 package com.example.csdnblog4;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
 /**
  * Created by orange on 16/2/18.
  */
-public class FeatureOne {
+public class FeatureOne extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
