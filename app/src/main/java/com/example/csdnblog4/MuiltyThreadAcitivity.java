517b5b9ba1e196c21dbdbc85bcf9c2baabe15db0
orange <huangcheng@cheyaoshi.com>
2016-06-07 21:20:13 +0800

    添加activity动画,以及使用统一样式

---
+++ app/src/main/java/com/example/csdnblog4/MuiltyThreadAcitivity.java
@@ -0,0 +1,12 @@
+package com.example.csdnblog4;
+
+/**
+ * Created by orange on 16/6/7.
+ */
+public class MuiltyThreadAcitivity extends BaseActivity {
+
+    @Override
+    int initContentView() {
+        return R.layout.muilty_thread_act;
+    }
+}
